6f9607e900d89bccc4bd32f32fb578d24a258a44
Juha Räisälä <juha.raisala@gmail.com>
2019-02-05 16:01:46 +0200

    Add /login route & /authentication

--- server/authentication/index.js
+++ server/authentication/index.js
@@ -1,19 +1,32 @@
 'use strict'
 
-async function register (server, options) {
-  // declare dependencies to hapi-auth-* plugins
-  await server.register([
-    // register hapi-auth-* plugins here
-  ])
+const Routes = require('./routes')
 
+
+
+exports.plugin = {
+	name: "cookie",
+	version: "1.0.0",
+	register: async server => {
+
+  // declare dependencies to hapi-auth-* plugins
+   server.route(Routes)
+
+	// register hapi-auth-* plugins here
+	await server.register([{
+	plugin: require('hapi-auth-cookie')
+	}
+	])
+	
   /**
    * Register cookie-based session auth to remember
    * the logged in user
    */
-}
-
-exports.plugin = {
-  name: 'authentication',
-  version: '1.0.0',
-  register
+		server.auth.strategy("session", "cookie", {
+		password: "password-should-be-32-characters",
+		cookie: "session",
+		isSecure: false,
+		redirectTo: "/login"
+		})
+	}
 }
--- server/base/routes.js
+++ server/base/routes.js
@@ -4,11 +4,11 @@ const Handler = require('./handler')
 
 const Routes = [
   {
-    method: 'GET',
-    path: '/',
-    config: Handler.index
-  },
-  {
+	method: 'GET',
+	path: '/',
+	config: Handler.index
+},
+{
     method: 'GET',
     path: '/js/{path*}',
     config: Handler.js
