51259780359986e38afcc911290dc7a284f32b3c
Juha Räisälä <juha.raisala@gmail.com>
2019-02-11 10:33:08 +0200

    API Pagination for TV Shows

--- api/tv-shows/handler.js
+++ api/tv-shows/handler.js
@@ -4,15 +4,32 @@ const Boom = require('boom')
 const Joi = require('joi')  
 const Path = require('path')  
 const Show = require(Path.resolve(__dirname, '..', '..', 'models')).Show
+const Paginator = require(Path.resolve(__dirname, '..', '..', 'utils', 'paginator'))
 
 const Handler = {  
   index: {
     handler: async (request, h) => {
-      const shows = await Show.find()
-
-      return shows
-    }
-  },
+		const totalCount = await Show.count()
+		const pagination = new Paginator(request, totalCount)
+  
+		if (pagination.currentPage > pagination.lastPage) {
+		  return Boom.notFound(
+			`The requested page does not exist. The last available page is: ${pagination.lastPage}`
+		  )
+		}
+  
+		const shows = await Show.find()
+		  .skip(pagination.from)
+		  .limit(pagination.perPage)
+  
+		return h.response(shows).header('Link', pagination.link)
+	  },
+	  validate: {
+		query: {
+		  page: Joi.number().integer().min(1)
+		}
+	  }
+	},
 
   show: {
     handler: async (request, h) => {
